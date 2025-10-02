22c172576d7f4669ec2d0ec61a9da3f4d6636a2d
IsaacQ5 <Isaacquevy5@gmail.com>
2025-10-02 16:24:07 -0400

    made all the peices on the board

--- main.py
+++ main.py
@@ -8,7 +8,26 @@ def pawns(board):
     for pawn in range(len(board[6])):
         board[6][pawn] = 'p'
 
+def otherpeices(board):
+    board[0][0] = 'r'
+    board[0][1] = 'n'
+    board[0][2] = 'b'
+    board[0][3] = 'q'
+    board[0][4] = 'k'
+    board[0][5] = 'b'
+    board[0][6] = 'n'
+    board[0][7] = 'r'
+
+    board[7][0] = 'r'
+    board[7][1] = 'n'
+    board[7][2] = 'b'
+    board[7][3] = 'q'
+    board[7][4] = 'k'
+    board[7][5] = 'b'
+    board[7][6] = 'n'
+    board[7][7] = 'r'
 def peices():
+    otherpeices(board)
     pawns(board)
 
 def displayboard(board):
