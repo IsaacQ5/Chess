3a6e2ff287675bff30f056f8a7acb3dbd6982a88
IsaacQ5 <Isaacquevy5@gmail.com>
2025-10-02 22:00:14 -0400

    Made the chess board easy to read

--- main.py
+++ main.py
@@ -3,32 +3,32 @@
 
 ROWS= 8
 COLS = 8
-board = [[' ' for _ in range(COLS)] for _ in range(ROWS)]
+board = [['--' for _ in range(COLS)] for _ in range(ROWS)]
 
 def pawns(board):
     for pawn in range(len(board[1])):
-        board[1][pawn] = 'p'
+        board[1][pawn] = 'BP'
     for pawn in range(len(board[6])):
-        board[6][pawn] = 'p'
+        board[6][pawn] = 'WP'
 
 def otherpeices(board):
-    board[0][0] = 'r'
-    board[0][1] = 'n'
-    board[0][2] = 'b'
-    board[0][3] = 'q'
-    board[0][4] = 'k'
-    board[0][5] = 'b'
-    board[0][6] = 'n'
-    board[0][7] = 'r'
+    board[0][0] = 'BR'
+    board[0][1] = 'BN'
+    board[0][2] = 'BB'
+    board[0][3] = 'BQ'
+    board[0][4] = 'BK'
+    board[0][5] = 'BB'
+    board[0][6] = 'BN'
+    board[0][7] = 'BR'
 
-    board[7][0] = 'r'
-    board[7][1] = 'n'
-    board[7][2] = 'b'
-    board[7][3] = 'q'
-    board[7][4] = 'k'
-    board[7][5] = 'b'
-    board[7][6] = 'n'
-    board[7][7] = 'r'
+    board[7][0] = 'WR'
+    board[7][1] = 'WN'
+    board[7][2] = 'WB'
+    board[7][3] = 'WQ'
+    board[7][4] = 'WK'
+    board[7][5] = 'WB'
+    board[7][6] = 'WN'
+    board[7][7] = 'WR'
 
 def peices():
     otherpeices(board)
